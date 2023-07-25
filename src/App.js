d96fb84f4598a15a15b2870bcc13cd832df3ee70
NicdDuck <pitushara06@gmail.com>
2023-07-25 16:23:58 +0300

    my changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { useEffect } from 'react'
 import './App.css'
-const tg = window.Telegram.WebApp
-import Header from './components/Header/Header'
+import {useEffect} from "react"
+import {useTelegram} from "./hooks/useTelegram"
+import Header from "./components/Header/Header"
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList"
+import Form from "./components/Form/Form"
 
 function App() {
+    const {onToggleButton, tg} = useTelegram()
 
-  useEffect (() => {
-    tg.ready()
-  }, [])
+    useEffect(() => {
+        tg.ready()
+    }, [])
 
-  return (
-    <div className="App">
-      work
-      <Header/>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    )
 }
 
 export default App
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,19 @@
-import React from "react"
+import React from 'react'
+import Button from "../Button/Button"
+import {useTelegram} from "../../hooks/useTelegram"
+import './Header.css'
 
 const Header = () => {
-    const tg = window.Telegram.WebApp
+    const {user, onClose} = useTelegram()
 
-    const onClose = () => {
-      tg.close()
-    }
-    return(
-        <div className={"header"}>
-            <Button onClick={onClose}>закрыть</Button>
-            <span className="username">
-                {tg.initDataUnsage?.user?.username}
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {user?.username}
             </span>
         </div>
-    )
-}
+    );
+};
 
 export default Header
